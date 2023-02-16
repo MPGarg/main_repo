33b0df059aeb074640a4dfa11aed20daaed9bb8c
MPGarg <120099863+MPGarg@users.noreply.github.com>
2023-02-16 13:45:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,143 @@
+from tqdm import tqdm
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch
+import torch.nn.functional as F
+import torchvision
+from torchsummary import summary
+import numpy as np
+from torch.optim.lr_scheduler import StepLR,OneCycleLR
+from torch.optim import Adam
+from torch.optim import SGD
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+def train(model, device, train_loader, optimizer, epoch, train_losses, train_acc,criterion, lambda_l1=0):
+
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    train_loss = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+        # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+
+        # Calculate loss
+        loss = criterion(y_pred, target)
+
+        if(lambda_l1 > 0):
+            l1 = 0
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+            loss = loss + lambda_l1*l1
+
+        train_loss += loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        # Update pbar-tqdm
+        
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()   
+    
+    train_losses.append(train_loss/len(train_loader.dataset))
+    train_acc.append(100*correct/len(train_loader.dataset))
+
+    print(f'\nAverage Training Loss={train_loss/len(train_loader.dataset)}, Accuracy={100*correct/len(train_loader.dataset)}')
+
+def test(model, device, test_loader,test_losses, test_acc,epoch,criterion,target_acc=95,save_file=''):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    accuracy_epoch = 100. * correct / len(test_loader.dataset)
+
+    if(accuracy_epoch > target_acc) and save_file == 'X':
+        model_name_file = "Model_" + str(epoch) + "_acc_" + str(round(accuracy_epoch,2)) + ".pth"
+        path = "/content/drive/MyDrive/" + model_name_file
+        torch.save(model.state_dict(), path)
+        print(f'Saved Model weights in file:  {model_name_file}')
+
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    return accuracy_epoch
+
+def fit_model(model, trainloader, testloader,criterion, EPOCHS, lr=0.001, device='cuda',sched='StepLR',lambda_l1=0,target_acc=100,optim='SGD'):
+    wrong_prediction_list = []
+    right_prediction_list = []
+    train_losses = []
+    train_acc = []
+    test_losses = []
+    test_acc = []
+
+    #torch.manual_seed(42)
+    if optim == 'SGD':
+        optimizer = SGD(model.parameters(), lr=lr, momentum=0.9)
+    elif optim == 'ADAM':
+        optimizer = Adam(model.parameters(), lr=lr, betas=(0.9, 0.999))
+    else:
+        optimizer = SGD(model.parameters(), lr=lr, momentum=0.9)      #fallback if nothing given
+
+    if sched == 'StepLR':
+        scheduler = StepLR(optimizer, step_size=100, gamma=0.25)    
+        sched_fl = 'X'
+    elif sched == 'OneCycle':
+        scheduler = OneCycleLR(optimizer=optimizer, max_lr=0.05, epochs=EPOCHS, steps_per_epoch=len(trainloader), pct_start=5/EPOCHS, div_factor=10) 
+        sched_fl = 'X'
+    else:
+        sched_fl = ' '
+
+    lambda_l1 = lambda_l1
+    
+    for epoch in range(EPOCHS):
+        print("EPOCH:", epoch+1)
+        train(model, device, trainloader, optimizer, epoch, criterion, train_losses, train_acc, lambda_l1)
+
+        if sched_fl == 'X':
+            scheduler.step()
+
+        eval_test_acc = test(model, device, testloader, test_losses, criterion, test_acc, epoch)
+        if(eval_test_acc >= target_acc):
+            break
+    
+    model.eval()
+    for images, labels in testloader:
+        images, labels = images.to(device), labels.to(device)
+        output = model(images)
+        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        match = pred.eq(labels.view_as(pred)).to('cpu').numpy()
+        for j, i in enumerate(match):
+            if(i == False):
+                wrong_prediction_list.append((images[j], pred[j].item(), labels[j].item()))
+            else:
+                right_prediction_list.append((images[j], pred[j].item(), labels[j].item()))
+
+    print(f'Total Number of incorrectly predicted images by model is {len(wrong_prediction_list)}')
+    return model, wrong_prediction_list, right_prediction_list, train_losses, train_acc, test_losses, test_acc
---
+++ utils.py
@@ -0,0 +1,291 @@
+from torchvision import datasets, transforms
+import torch
+import torch.nn.functional as F
+import torchvision
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+class cifar_ds10(torchvision.datasets.CIFAR10):
+    def __init__(self, root="./data", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+
+        return image, label
+
+# custom dataset class for albumentations library
+class AlbumentationImageDataset(Dataset):
+    def __init__(self, image_list, mean, std, train= True):
+        self.image_list = image_list
+
+        self.aug = A.Compose({
+            A.Normalize(mean, std),
+            A.PadIfNeeded(min_height=40, min_width=40, always_apply=True),
+            A.RandomCrop(width=32, height=32),
+            A.HorizontalFlip(p=0.2),
+            A.CoarseDropout(max_holes=1,min_holes = 1, max_height=8, max_width=8, p=0.5,fill_value=np.mean(mean), min_height=8, min_width=8, mask_fill_value = None)          
+        })
+
+        self.norm = A.Compose({A.Normalize(mean, std),
+        })
+        self.train = train
+            
+    def __len__(self):
+        return (len(self.image_list))
+
+    def __getitem__(self, i):
+        
+        image, label = self.image_list[i]
+        
+        if self.train:
+            #apply augmentation only for training
+            image = self.aug(image=np.array(image))['image']
+        else:
+            image = self.norm(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        return torch.tensor(image, dtype=torch.float), label    
+
+def viz_data(exp, cols=8, rows=5):
+    figure = plt.figure(figsize=(14, 10))
+    for i in range(1, cols * rows + 1):
+        img, label = exp[i]
+
+        figure.add_subplot(rows, cols, i)
+        plt.title(exp.classes[label])
+        plt.axis("off")
+        plt.imshow(img, cmap="gray")
+
+    plt.tight_layout()
+    plt.show() 
+    
+def process_dataset(batch_size=512,visualize = ''):
+    trainset = cifar_ds10(root='./data', train=True, download=True)
+    testset = cifar_ds10(root='./data', train=False, download=True)
+
+    mean = np.mean(trainset, axis=(0,1,2)) / 255.
+    std = np.std(trainset, axis=(0,1,2)) / 255.
+
+    if visualize == 'X':
+        viz_data(trainset)
+
+    trainloader = torch.utils.data.DataLoader(AlbumentationImageDataset(trainset,mean,std, train=True), batch_size=batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(AlbumentationImageDataset(testset,mean,std, train=False), batch_size=batch_size, shuffle=False, num_workers=2)
+
+    return trainset,trainloader,testset,testloader , mean, std 
+
+def plot_acc_loss(train_acc,train_losses,test_acc,test_losses):
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+
+    axs[0].plot(train_losses, label='Training Losses')
+    axs[0].plot(test_losses, label='Test Losses')
+    axs[0].legend(loc='upper right')
+    axs[0].set_xlabel('Epochs')
+    axs[0].set_ylabel('Loss')
+    axs[0].set_title("Loss")
+
+    axs[1].plot(train_acc, label='Training Accuracy')
+    axs[1].plot(test_acc, label='Test Accuracy')
+    axs[1].legend(loc='lower right')
+    axs[1].set_xlabel('Epochs')
+    axs[1].set_ylabel('Accuracy')
+    axs[1].set_title("Accuracy")
+
+    plt.show()    
+
+def display_incorrect_pred(mismatch, n=20 ):
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    display_images = mismatch[:n]
+    index = 0
+    fig = plt.figure(figsize=(20,20))
+    for img in display_images:
+        image = img[0].squeeze().to('cpu').numpy()
+        pred = classes[img[1]]
+        actual = classes[img[2]]
+        ax = fig.add_subplot(4, 5, index+1)
+        ax.axis('off')
+        ax.set_title(f'\n Predicted Label : {pred} \n Actual Label : {actual}',fontsize=10) 
+        ax.imshow(np.transpose(image, (1, 2, 0))) 
+        #ax.imshow(image, cmap='gray_r')
+        index = index + 1
+    plt.show()
+
+#GradCam copied from https://github.com/kazuto1011/grad-cam-pytorch
+
+from torch.nn import functional as F
+import cv2
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+class GradCAM:
+    """ Class for extracting activations and 
+    registering gradients from targetted intermediate layers 
+    target_layers = list of convolution layer index as shown in summary
+    """
+    def __init__(self, model, candidate_layers=None):
+        def save_fmaps(key):
+          def forward_hook(module, input, output):
+              self.fmap_pool[key] = output.detach()
+
+          return forward_hook
+
+        def save_grads(key):
+          def backward_hook(module, grad_in, grad_out):
+              self.grad_pool[key] = grad_out[0].detach()
+
+          return backward_hook
+
+        self.device = next(model.parameters()).device
+        self.model = model
+        self.handlers = []  # a set of hook function handlers
+        self.fmap_pool = {}
+        self.grad_pool = {}
+        self.candidate_layers = candidate_layers  # list
+
+        for name, module in self.model.named_modules():
+            if self.candidate_layers is None or name in self.candidate_layers:
+                self.handlers.append(module.register_forward_hook(save_fmaps(name)))
+                self.handlers.append(module.register_backward_hook(save_grads(name)))
+
+    def _encode_one_hot(self, ids):
+        one_hot = torch.zeros_like(self.nll).to(self.device)
+        print(one_hot.shape)
+        one_hot.scatter_(1, ids, 1.0)
+        return one_hot
+
+    def forward(self, image):
+        self.image_shape = image.shape[2:] # HxW
+        self.nll = self.model(image)
+        #self.probs = F.softmax(self.logits, dim=1)
+        return self.nll.sort(dim=1, descending=True)  # ordered results
+
+    def backward(self, ids):
+        """
+        Class-specific backpropagation
+        """
+        one_hot = self._encode_one_hot(ids)
+        self.model.zero_grad()
+        self.nll.backward(gradient=one_hot, retain_graph=True)
+
+    def remove_hook(self):
+        """
+        Remove all the forward/backward hook functions
+        """
+        for handle in self.handlers:
+            handle.remove()
+
+    def _find(self, pool, target_layer):
+        if target_layer in pool.keys():
+            return pool[target_layer]
+        else:
+            raise ValueError("Invalid layer name: {}".format(target_layer))
+
+    def generate(self, target_layer):
+        fmaps = self._find(self.fmap_pool, target_layer)
+        grads = self._find(self.grad_pool, target_layer)
+        weights = F.adaptive_avg_pool2d(grads, 1)
+
+        gcam = torch.mul(fmaps, weights).sum(dim=1, keepdim=True)
+        gcam = F.relu(gcam)
+        # need to capture image size duign forward pass
+        gcam = F.interpolate(
+            gcam, self.image_shape, mode="bilinear", align_corners=False
+        )
+
+        # scale output between 0,1
+        B, C, H, W = gcam.shape
+        gcam = gcam.view(B, -1)
+        gcam -= gcam.min(dim=1, keepdim=True)[0]
+        gcam /= gcam.max(dim=1, keepdim=True)[0]
+        gcam = gcam.view(B, C, H, W)
+
+        return gcam
+
+def generate_gradcam(misclassified_images, model, target_layers,device):
+    images=[]
+    labels=[]
+    for i, (img, pred, correct) in enumerate(misclassified_images):
+        images.append(img)
+        labels.append(correct)
+    
+    model.eval()
+    
+    # map input to device
+    images = torch.stack(images).to(device)
+    
+    # set up grad cam
+    gcam = GradCAM(model, target_layers)
+    
+    # forward pass
+    probs, ids = gcam.forward(images)
+    
+    # outputs agaist which to compute gradients
+    ids_ = torch.LongTensor(labels).view(len(images),-1).to(device)
+    
+    # backward pass
+    gcam.backward(ids=ids_)
+    layers = []
+    for i in range(len(target_layers)):
+        target_layer = target_layers[i]
+        print("Generating Grad-CAM @{}".format(target_layer))
+        # Grad-CAM
+        layers.append(gcam.generate(target_layer=target_layer))
+        
+    # remove hooks when done
+    gcam.remove_hook()
+    return layers, probs, ids
+
+def plot_gradcam(gcam_layers, target_layers, class_names, image_size,predicted, misclassified_images,mean,std):
+
+    images=[]
+    labels=[]
+    for i, (img, pred, correct) in enumerate(misclassified_images):
+      images.append(img)
+      labels.append(correct)
+
+    c = len(images)+1
+    r = len(target_layers)+2
+    fig = plt.figure(figsize=(30,14))
+    fig.subplots_adjust(hspace=0.01, wspace=0.01)
+    ax = plt.subplot(r, c, 1)
+    ax.text(0.3,-0.5, "INPUT", fontsize=14)
+    plt.axis('off')
+    for i in range(len(target_layers)):
+      target_layer = target_layers[i]
+      ax = plt.subplot(r, c, c*(i+1)+1)
+      ax.text(0.3,-0.5, target_layer, fontsize=14)
+      plt.axis('off')
+
+      for j in range(len(images)):
+        image_cpu = images[j].cpu().numpy().astype(dtype=np.float32)
+        for k in range(image_cpu.shape[0]):
+          image_cpu[k] = image_cpu[k] * std[k] + mean[k]
+        image_cpu = np.transpose(image_cpu, (1,2,0))
+        img = np.uint8(255*image_cpu)
+        if i==0:
+          ax = plt.subplot(r, c, j+2)
+          ax.text(0, 0.2, f"actual: {class_names[labels[j]]} \npredicted: {class_names[predicted[j][0]]}", fontsize=12)
+          plt.axis('off')
+          plt.subplot(r, c, c+j+2)
+          plt.imshow(img)
+          plt.axis('off')
+          
+        
+        heatmap = 1-gcam_layers[i][j].cpu().numpy()[0] # reverse the color map
+        heatmap = np.uint8(255 * heatmap)
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        superimposed_img = cv2.resize(cv2.addWeighted(img, 0.5, heatmap, 0.5, 0), (128,128))
+        plt.subplot(r, c, (i+2)*c+j+2)
+        plt.imshow(superimposed_img, interpolation='bilinear')
+        
+        plt.axis('off')
+    plt.show()
+     
