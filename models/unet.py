7fd65584bc06a8dc0432c21ea5cdee26f23fdc71
MPGarg <120099863+MPGarg@users.noreply.github.com>
2023-04-13 23:54:31 +0530

    Add files via upload

--- models/unet.py
+++ models/unet.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 
 class ContractingBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, pool='max'):
         super(ContractingBlock, self).__init__()
         
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
@@ -14,6 +14,9 @@ class ContractingBlock(nn.Module):
         self.relu2 = nn.ReLU(inplace=True)
         
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.StrConv = nn.Conv2d(out_channels, out_channels, kernel_size=2, stride=2, padding=0)
+
+        self.pool = pool
         
     def forward(self, x):
         x = self.conv1(x)
@@ -25,7 +28,11 @@ class ContractingBlock(nn.Module):
         x = self.relu2(x)
         
         skip = x  # store the output for the skip connection
-        x = self.maxpool(x)
+
+        if self.pool == 'max':
+            x = self.maxpool(x)
+        else:
+            x = self.StrConv(x)
         
         return x, skip
 
@@ -42,7 +49,7 @@ class ExpandingBlock(nn.Module):
         self.relu2 = nn.ReLU(inplace=True)
         
         self.r = r
-        if mode=='transpose':
+        if mode == 'transpose':
             self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
         else:
             self.upsample = nn.Sequential(nn.Upsample(scale_factor=2, mode='nearest'),
@@ -68,19 +75,19 @@ class ExpandingBlock(nn.Module):
         return x
 
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels,mode='transpose',r=False):
+    def __init__(self, in_channels, out_channels,mode='transpose',r=False,pool='max'):
         super(UNet, self).__init__()
         
-        self.contract1 = ContractingBlock(in_channels, 64)
-        self.contract2 = ContractingBlock(64, 128)
-        self.contract3 = ContractingBlock(128, 256)
-        self.contract4 = ContractingBlock(256, 512)
+        self.contract1 = ContractingBlock(in_channels, 64,pool)
+        self.contract2 = ContractingBlock(64, 128,pool)
+        self.contract3 = ContractingBlock(128, 256,pool)
+        self.contract4 = ContractingBlock(256, 512,pool)
      
         self.expand1 = ExpandingBlock(512, 256,mode)
         self.expand2 = ExpandingBlock(256, 128,mode)
         self.expand3 = ExpandingBlock(128, 64,mode,r)
 
-        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)        
         
     def forward(self, x):
         # Contracting path
