0e8f87703011205bf4225fbbc5ef7ac13dd6fe50
MPGarg <120099863+MPGarg@users.noreply.github.com>
2023-04-13 12:55:26 +0530

    Add files via upload

--- models/unet.py
+++ models/unet.py
@@ -17,11 +17,11 @@ class ContractingBlock(nn.Module):
         
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
+        #x = self.bn1(x)
         x = self.relu1(x)
         
         x = self.conv2(x)
-        x = self.bn2(x)
+        #x = self.bn2(x)
         x = self.relu2(x)
         
         skip = x  # store the output for the skip connection
@@ -54,13 +54,13 @@ class ExpandingBlock(nn.Module):
         x = torch.cat((x, skip), dim=1)
 
         x = self.conv1(x)
-        x = self.bn1(x)
+        #x = self.bn1(x)
 
         if self.r:
             x = self.relu1(x)
         
         x = self.conv2(x)
-        x = self.bn2(x)
+        #x = self.bn2(x)
 
         if self.r:
             x = self.relu2(x)        
@@ -75,12 +75,10 @@ class UNet(nn.Module):
         self.contract2 = ContractingBlock(64, 128)
         self.contract3 = ContractingBlock(128, 256)
         self.contract4 = ContractingBlock(256, 512)
-        self.contract5 = ContractingBlock(512, 1024)
-
-        self.expand1 = ExpandingBlock(1024, 512,mode)        
-        self.expand2 = ExpandingBlock(512, 256,mode)
-        self.expand3 = ExpandingBlock(256, 128,mode)
-        self.expand4 = ExpandingBlock(128, 64,mode,r)
+     
+        self.expand1 = ExpandingBlock(512, 256,mode)
+        self.expand2 = ExpandingBlock(256, 128,mode)
+        self.expand3 = ExpandingBlock(128, 64,mode,r)
 
         self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
         
@@ -89,14 +87,13 @@ class UNet(nn.Module):
         x, skip1 = self.contract1(x)
         x, skip2 = self.contract2(x)
         x, skip3 = self.contract3(x)
-        x, skip4 = self.contract4(x)
-        _, x = self.contract5(x)
+        _, x = self.contract4(x)
         
         # Expanding path
-        x = self.expand1(x, skip4)
-        x = self.expand2(x, skip3)
-        x = self.expand3(x, skip2)
-        x = self.expand4(x, skip1)
+        x = self.expand1(x, skip3)
+        x = self.expand2(x, skip2)
+        x = self.expand3(x, skip1)
 
-        return self.final_conv(x)
+        x = self.final_conv(x)
+        return x
      
